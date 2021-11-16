41e0908f0fb4fb5501b73119c97dec41ca4c5f9f
Stefan Sundin <git@stefansundin.com>
2021-11-16 12:37:12 -0800

    Add some tokens (they are not valid anymore).

--- test.rb
+++ test.rb
@@ -52,3 +52,20 @@ def valid_checksum?(token)
 
   SecurityUtils.secure_compare(calculated_checksum, provided_checksum)
 end
+
+def test(token)
+  puts "Testing #{token} => #{valid_checksum?(token)}"
+end
+
+puts "Tokens I generated on GitHub:"
+test("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")
+test("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")
+test("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")
+test("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")
+
+puts
+puts "Fake tokens that I made myself:"
+test("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")
+test("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")
+test("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")
+test("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")
